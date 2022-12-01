cabd9af339124ee800d71c05e56d66f2189f4e7d
Bibin Panicker <bibinpanicker07@gmail.com>
2022-12-01 13:22:33 +0530

    bibin

---
+++ src/main/java/com/axis/groceryShopBackend/exception/ProductNotFoundException.java
@@ -0,0 +1,19 @@
+package com.axis.groceryShopBackend.exception;
+
+public class ProductNotFoundException extends RuntimeException{
+
+
+	private static final long serialVersionUID = 2176286812567782403L;
+
+	public ProductNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProductNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
